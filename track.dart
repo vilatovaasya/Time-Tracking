68a69f1205e33fe9f3078b85b2c22a981b38e157
Asya Vilatova <162153788+vilatovaasya@users.noreply.github.com>
2024-03-04 18:12:03 +0200

    Create track.dart

---
+++ track.dart
@@ -0,0 +1,23 @@
+export 'src/generated/pylons/item.pb.dart';
+export 'src/generated/pylons/execution.pb.dart';
+export 'src/generated/pylons/trade.pb.dart';
+export 'src/generated/pylons/tx.pb.dart';
+
+export 'src/features/data/models/profile.dart' show Profile;
+export 'src/features/helper/dec_string.dart';
+
+
+  group('Wallet.getProfile', () {
+    test("Throws a NoWalletException if there's no wallet", () {
+      throw UnimplementedError('TODO');
+    });
+    test('Throws a NotAnAddressException if given a bad address', () {
+      throw UnimplementedError('TODO');
+    });
+    test("When there's no argument, get the active profile", () {
+      throw UnimplementedError('TODO');
+    });
+    test("When there's a valid address, get the correct profile", () {
+      throw UnimplementedError('TODO');
+    });
+  });
